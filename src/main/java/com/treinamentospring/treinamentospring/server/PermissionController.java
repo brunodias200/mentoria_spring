57e9b6a9aa452eedf870edc5781d55aa45e49f80
Your Name <you@example.com>
2024-03-04 18:47:16 -0300

    criada 1ª rota do tipo GET

--- src/main/java/com/treinamentospring/treinamentospring/server/PermissionController.java
+++ src/main/java/com/treinamentospring/treinamentospring/server/PermissionController.java
@@ -6,12 +6,16 @@ import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/permissions")
 public class PermissionController {
     private final PermissionService service;
+    @GetMapping("/name/{name}")
     public ResponseEntity<Page<PermissionModel>> findByName(String name, Pageable pageable){
         return ResponseEntity.ok(service.findByNameContainsIgnoreCase(name, pageable));
     }
