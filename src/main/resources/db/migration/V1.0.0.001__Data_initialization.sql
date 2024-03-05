ed89932136d9fa03a6d1b9a4e69c4fab083401ec
Bruno Dias <brunoadias1996@gmail.com>
2024-03-05 18:46:50 -0300

    ajuste nas migration

--- src/main/resources/db/migration/V1.0.0.001__Data_initialization.sql
+++ src/main/resources/db/migration/V1.0.0.001__Data_initialization.sql
@@ -14,3 +14,11 @@ INSERT INTO `profile` (`name`, `created_at`, `modified_at`, `enabled`) VALUES
     ('Administrator', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, TRUE);
 
 INSERT INTO `profile_permission` SELECT pro.`id`, per.`id` FROM `profile` pro INNER JOIN `permission` per;
+
+INSERT INTO `profile` (`name`, `created_at`, `modified_at`, `enabled`) VALUES
+    ('Read only', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, TRUE);
+
+INSERT INTO `profile_permission` (profile_id, permission_id) VALUES
+    ((SELECT pro.`id` FROM `profile` pro WHERE pro.`name` = 'Read only'), (SELECT per.`id` FROM `permission` per WHERE per.`name` = 'PERMISSION_READ')),
+    ((SELECT pro.`id` FROM `profile` pro WHERE pro.`name` = 'Read only'), (SELECT per.`id` FROM `permission` per WHERE per.`name` = 'PROFILE_READ')),
+    ((SELECT pro.`id` FROM `profile` pro WHERE pro.`name` = 'Read only'), (SELECT per.`id` FROM `permission` per WHERE per.`name` = 'USER_READ'));
